6a36bd1c27f2b1e958f02cec4fe979cda59bd688
Pedro Torres <pedrohagatorres@gmail.com>
2017-09-26 17:04:25 -0300

    Criação do objeto GameEngine.

---
+++ Batalha/src/main/scala/GameEngine.scala
@@ -0,0 +1,18 @@
+object GameEngine {
+  var coin : Int = -1
+
+  coin = GameInterface.pushStartButton
+
+  while (coin == 1) {
+
+    if (coin == 1)
+      //PositionShips
+
+    coin = GameInterface.insertCoin()
+
+    if (coin == 0)
+      GameInterface.gameOver()
+  }
+
+  GameInterface.endGame()
+}
