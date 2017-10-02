87f8203604808110ead3ab1c3481f5e356ebb2f8
Pedro Torres <pedrohagatorres@gmail.com>
2017-10-02 17:33:23 -0300

    Mudança na def setQuant para decreaseQuant.

--- Batalha/src/main/scala/Ships.scala
+++ Batalha/src/main/scala/Ships.scala
@@ -1,7 +1,7 @@
-class Ships(val name: String, val desig: Char, val size: Int, var quant: Int, var isdown: Boolean) {
+class Ships(val name: String, val desig: Char, val size: Int, var quant: Int, var isdown: Boolean = false) {
 
-def setQuant (i : Int): Unit = {
-  quant = i
+def decreaseQuant(): Unit = {
+  quant -= 1
 }
 
 def setState (b : Boolean): Unit = {
