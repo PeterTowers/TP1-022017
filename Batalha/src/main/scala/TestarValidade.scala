c5b634f924aa63ee8d53a13e96ba5a5c2396d3ee
Pedro Torres <pedrohagatorres@gmail.com>
2017-10-03 17:56:38 -0300

    Adição de dois novos testes.

--- Batalha/src/main/scala/TestarValidade.scala
+++ Batalha/src/main/scala/TestarValidade.scala
@@ -8,18 +8,64 @@ object TestarValidade {
       retorno = scala.io.StdIn.readInt()
     }
 
-    return(retorno)
+    return retorno
   }
 
-  def maiorMenor (alvo : Int, isso : Int, aquilo : Int): Int = {
+  def maiorMenor (alvo: Int, isso: Int, aquilo: Int): Int = {
     var retorno = alvo
 
-    while (retorno > isso || retorno < aquilo) {
-      println("Entrada invalida. Digite um valor entre " + aquilo + " e " + isso +".")
+    while (retorno < isso || retorno > aquilo) {
+      println("Entrada invalida. Digite um valor entre " + isso + " e " + aquilo +".")
       retorno = scala.io.StdIn.readInt()
     }
 
-    return(retorno)
+    return retorno
+  }
+
+  def horizontalVertical (alvo: Int): Int = {
+    var retorno = alvo
+
+    while (retorno != 1 && retorno != 2) {
+      println("Entrada invalida, digite 1 para posicionar na vertical ou 0 para horizontal.")
+      retorno = scala.io.StdIn.readInt()
+    }
+    return retorno
+  }
+
+  def testaPosicionamento (current: Ships, array: Array[Array[Array[Char]]], direction: Int, x: Int, y: Int): Boolean = {
+    var flag = false
+
+    if (direction == 1) {
+      if ( (y+current.size) > PositionShips.ysize ) {
+        flag = true
+        println("Sua entrada eh invalida, pois parte do seu navio ficaria fora do tabuleiro. Tente novamente.")
+      }
+      else {
+        for (j <- y until (y + current.size)) {
+          if (array(j)(x)(0) != '~') {
+            flag = true
+            println("Sua entrada eh invalida, pois seu este navio ficaria em uma posicao ja ocupada por outro. Tente novamente.")
+            return flag
+          }
+        }
+      }
+    }
+    else {
+      if ( (x+current.size) > PositionShips.xsize ) {
+        flag = true
+        println("Sua entrada eh invalida, pois parte do seu navio ficaria fora do tabuleiro. Tente novamente.")
+      }
+      else {
+        for (j <- x until (x + current.size)) {
+          if (array(y)(j)(0) != '~') {
+            flag = true
+            println("Sua entrada eh invalida, pois seu este navio ficaria em uma posicao ja ocupada por outro. Tente novamente.")
+            return flag
+          }
+        }
+      }
+    }
+    return flag
   }
 
 }
