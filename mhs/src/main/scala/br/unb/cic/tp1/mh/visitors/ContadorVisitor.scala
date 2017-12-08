1b925d4a0237e975d97b73bfe1e591518a166c6c
Pedro Torres <pedrohagatorres@gmail.com>
2017-12-08 19:07:32 -0200

    Implementação de métodos para algumas expressões.

--- mhs/src/main/scala/br/unb/cic/tp1/mh/visitors/ContadorVisitor.scala
+++ mhs/src/main/scala/br/unb/cic/tp1/mh/visitors/ContadorVisitor.scala
@@ -5,15 +5,89 @@ class ContadorVisitor extends Visitor {
 
   var contador = 0
 
+  /* -----Tipos de variaveis----- */
+
   override def visitar(exp: ValorInteiro): Unit = contador += 1
 
+  override def visitar(exp: ValorFloat): Unit = contador += 1
+
   override def visitar(exp: ValorBooleano): Unit = contador += 1
+
+  override def visitar(exp: ValorVazio): Unit = { } // Deve ser vazio, mesmo, pois a expressao ValorVazio nao faz nada.
+
+  /* ---------------------------------------------------------------------------------------------------------------- */
+  /* -----Expressoes matematicas----- */
 
   override def visitar(exp: ExpMatSoma): Unit = {
     exp.lhs.aceitar(this)
     exp.rhs.aceitar(this)
     contador += 1
   }
+
+  override def visitar(exp: ExpMatSubt): Unit = {
+    exp.lhs.aceitar(this)
+    exp.rhs.aceitar(this)
+    contador += 1
+  }
+
+  override def visitar(exp: ExpMatMult): Unit = {
+    exp.lhs.aceitar(this)
+    exp.rhs.aceitar(this)
+    contador += 1
+  }
+
+  override def visitar(exp: ExpMatDiv): Unit = {
+    exp.lhs.aceitar(this)
+    exp.rhs.aceitar(this)
+    contador += 1
+  }
+
+  /* ---------------------------------------------------------------------------------------------------------------- */
+  /* -----Expressoes logicas----- */
+
+  override def visitar(exp: ExpLogAnd): Unit = {
+    exp.lhs.aceitar(this)
+    exp.rhs.aceitar(this)
+    contador += 1
+  }
+
+  override def visitar(exp: ExpLogNot): Unit = contador += 1
+
+  override def visitar(exp: ExpLogOr): Unit = {
+    exp.lhs.aceitar(this)
+    exp.rhs.aceitar(this)
+    contador += 1
+  }
+
+  override def visitar(exp: ExpIfThenElse): Unit = {}
+
+  /* ---------------------------------------------------------------------------------------------------------------- */
+  /* -----Expressoes relacionais----- */
+
+  override def visitar(exp: ExpRelIgual): Unit = {
+    exp.lhs.aceitar(this)
+    exp.rhs.aceitar(this)
+    contador += 1
+  }
+
+  override def visitar(exp: ExpRelMaior): Unit = {
+    exp.lhs.aceitar(this)
+    exp.rhs.aceitar(this)
+    contador += 1
+  }
+
+  override def visitar(exp: ExpRelMaiorIg): Unit = {
+    exp.lhs.aceitar(this)
+    exp.rhs.aceitar(this)
+    contador += 1
+  }
+
+  override def visitar(exp: ExpRelMenor): Unit = { }    // Vazio, pois utiliza ExpRelMaior para determinar seu retorno.
+
+  override def visitar(exp: ExpRelMenorIg): Unit = { }  // Vazio, pois utiliza ExpRelMaiorIg para determinar seu retorno.
+
+  /* ---------------------------------------------------------------------------------------------------------------- */
+  /* -----Funcoes----- */
 
   override def visitar(exp: ExpLet): Unit = {
     exp.expNomeada.aceitar(this)
@@ -44,25 +118,5 @@ class ContadorVisitor extends Visitor {
     contador += 1
   }
 
-  override def visitar(exp: ValorFloat): Unit = {}
-
-  override def visitar(exp: ExpLogAnd): Unit = {}
-
-  override def visitar(exp: ExpIfThenElse): Unit = {}
-
-  override def visitar(exp: ExpLogNot): Unit = {}
-
-  override def visitar(exp: ExpLogOr): Unit = {}
-
-  override def visitar(exp: ExpMatMult): Unit = {}
-
-  override def visitar(exp: ExpRelIgual): Unit = {}
-
-  override def visitar(exp: ExpRelMaior): Unit = {}
-
-  override def visitar(exp: ExpRelMaiorIg): Unit = {}
-
-  override def visitar(exp: ExpRelMenor): Unit = {}
-
-  override def visitar(exp: ExpRelMenorIg): Unit = {}
+  /* ---------------------------------------------------------------------------------------------------------------- */
 }
--- mhs/src/main/scala/br/unb/cic/tp1/mh/visitors/PPVisitor.scala
+++ mhs/src/main/scala/br/unb/cic/tp1/mh/visitors/PPVisitor.scala
@@ -5,13 +5,18 @@ class PPVisitor extends Visitor {
 
   val sb = new StringBuilder("")
 
-  override def visitar(exp: ValorInteiro): Unit = {
-    sb.append(exp.v.toString)
-  }
+  /* -----Tipos de variaveis----- */
 
-  override def visitar(exp: ValorBooleano): Unit = {
-    sb.append(exp.v.toString)
-  }
+  override def visitar(exp: ValorInteiro): Unit = sb.append(exp.v.toString)
+
+  override def visitar(exp: ValorFloat): Unit = sb.append(exp.v.toString)
+
+  override def visitar(exp: ValorBooleano): Unit = sb.append(exp.v.toString)
+
+  override def visitar(exp: ValorVazio): Unit = { } // Nao deve fazer nada, pois ValorVazio nao faz nada.
+
+  /* ---------------------------------------------------------------------------------------------------------------- */
+  /* -----Expressoes matematicas----- */
 
   override def visitar(exp: ExpMatSoma): Unit = {
     sb += '('
@@ -20,6 +25,90 @@ class PPVisitor extends Visitor {
     exp.rhs.aceitar(this)
     sb += ')'
   }
+
+  override def visitar(exp: ExpMatSubt): Unit = {
+    sb += '('
+    exp.lhs.aceitar(this)
+    sb += '-'
+    exp.rhs.aceitar(this)
+    sb += ')'
+  }
+
+  override def visitar(exp: ExpMatMult): Unit = {
+    sb += '('
+    exp.lhs.aceitar(this)
+    sb += '*'
+    exp.rhs.aceitar(this)
+    sb += ')'}
+
+  override def visitar(exp: ExpMatDiv): Unit = {
+    sb += '('
+    exp.lhs.aceitar(this)
+    sb += '/'
+    exp.rhs.aceitar(this)
+    sb += ')'
+  }
+
+  /* ---------------------------------------------------------------------------------------------------------------- */
+  /* -----Expressoes logicas----- */
+
+  override def visitar(exp: ExpLogAnd): Unit = {
+    sb += '('
+    exp.lhs.aceitar(this)
+    sb.append("AND")
+    exp.rhs.aceitar(this)
+    sb += ')'
+  }
+
+  override def visitar(exp: ExpLogNot): Unit = {
+    sb += '('
+    exp.aceitar(this)
+    sb += ')'
+  }
+
+  override def visitar(exp: ExpLogOr): Unit = {
+    sb += '('
+    exp.lhs.aceitar(this)
+    sb.append("OR")
+    exp.rhs.aceitar(this)
+    sb += ')'
+  }
+
+  override def visitar(exp: ExpIfThenElse): Unit = { }
+
+  /* ---------------------------------------------------------------------------------------------------------------- */
+  /* -----Expressoes relacionais----- */
+
+  override def visitar(exp: ExpRelIgual): Unit = {
+    sb += '('
+    exp.lhs.aceitar(this)
+    sb.append("==")
+    exp.rhs.aceitar(this)
+    sb += ')'
+  }
+
+  override def visitar(exp: ExpRelMaior): Unit = {
+    sb += '('
+    exp.lhs.aceitar(this)
+    sb.append(">")
+    exp.rhs.aceitar(this)
+    sb += ')'
+  }
+
+  override def visitar(exp: ExpRelMaiorIg): Unit = {
+    sb += '('
+    exp.lhs.aceitar(this)
+    sb.append(">=")
+    exp.rhs.aceitar(this)
+    sb += ')'
+  }
+
+  override def visitar(exp: ExpRelMenor): Unit = { }    // Vazio pois seu funcionamento eh implementado por ExpRelMaior.
+
+  override def visitar(exp: ExpRelMenorIg): Unit = { }  // Vazio pois seu funcionamento eh implementado por ExpRelMaiorIg.
+
+  /* ---------------------------------------------------------------------------------------------------------------- */
+  /* -----Funcoes----- */
 
   override def visitar(exp: ExpLet): Unit = { }
 
@@ -33,25 +122,5 @@ class PPVisitor extends Visitor {
 
   override def visitar(exp: Closure): Unit = { }
 
-  override def visitar(exp: ValorFloat): Unit = {}
-
-  override def visitar(exp: ExpLogAnd): Unit = {}
-
-  override def visitar(exp: ExpIfThenElse): Unit = {}
-
-  override def visitar(exp: ExpLogNot): Unit = {}
-
-  override def visitar(exp: ExpLogOr): Unit = {}
-
-  override def visitar(exp: ExpMatMult): Unit = {}
-
-  override def visitar(exp: ExpRelIgual): Unit = {}
-
-  override def visitar(exp: ExpRelMaior): Unit = {}
-
-  override def visitar(exp: ExpRelMaiorIg): Unit = {}
-
-  override def visitar(exp: ExpRelMenor): Unit = {}
-
-  override def visitar(exp: ExpRelMenorIg): Unit = {}
+  /* ---------------------------------------------------------------------------------------------------------------- */
 }
